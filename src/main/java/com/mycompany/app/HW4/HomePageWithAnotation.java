722a5c8cce630f405dc0272c759d178a757f8ce5
Alexandr Horoshko <AlexandrHoroshko11@gmail.com>
2019-04-17 19:16:32 +0300

    Add annotation

---
+++ src/main/java/com/mycompany/app/HW4/HomePageWithAnotation.java
@@ -0,0 +1,55 @@
+package com.mycompany.app.HW4;
+
+import com.mycompany.app.HW3.DriverPoolHolder;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePageWithAnotation {
+
+    private WebDriver driver;
+    private static final String url = "https://google.com";
+
+    public HomePageWithAnotation(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    @FindBy (name="q")
+    WebElement SearchInput;
+
+    @FindBy(css = "[jsname = VlcLAe] center input[name = btnK]")
+    WebElement SearchButton;
+
+    @FindBy(css = ".FPdoLc>center>input[name = btnI]")
+    WebElement GoodLuckEmpty;
+
+    @FindBy(css = "[jsname = VlcLAe] center input[name = btnI]")
+    WebElement GoodLuckFillq;
+
+    public void open() throws Exception {
+        driver.get(url);
+    }
+
+
+    public void setText(String text) throws Exception {
+        SearchInput.sendKeys(text);
+    }
+
+//    public void submitSearch() throws Exception {
+//        DriverPoolHolder.getDriver("chrome").findElement(SearchInputLocator).submit();
+//    }
+//
+//    public void clickOnSearchButton() throws Exception {
+//        DriverPoolHolder.getDriver("chrome").findElement(SearchButtonLocator).click();
+//    }
+//
+//    public void clickOnGoodLuckEmpty() throws Exception {
+//        DriverPoolHolder.getDriver("chrome").findElement(GoodLuckEmptyqLocator).click();
+//    }
+//
+//    public void clickOnGoodLuck() throws Exception {
+//        DriverPoolHolder.getDriver("chrome").findElement(GoodLuckFillqLocator).click();
+//    }
+
+}
